5edc00ca499b041b9ef55a076c060f89aa2e3127
wb-wzt445153 <wb-wzt445153@alibaba-inc.com>
2019-01-11 11:31:46 +0800

    首次提交代码

---
+++ src/main/java/com/tao/wztsnn/WztsnnApplication.java
@@ -0,0 +1,14 @@
+package com.tao.wztsnn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WztsnnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WztsnnApplication.class, args);
+    }
+
+}
+
