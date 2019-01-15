f3ce8c3b36c6c3bb9fa7f3222c5364a1a6ea0694
wb-wzt445153 <wb-wzt445153@alibaba-inc.com>
2019-01-15 17:29:42 +0800

    增加测试controller

---
+++ src/main/java/com/tao/wztsnn/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.tao.wztsnn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+    @RequestMapping("test")
+    public @ResponseBody String test(){
+        return "test good";
+    }
+}
