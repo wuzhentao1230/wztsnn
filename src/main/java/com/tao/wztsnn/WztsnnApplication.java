9eb60df0883ff6163aef7b3cb59f27614991ecd3
wb-wzt445153 <wb-wzt445153@alibaba-inc.com>
2019-01-15 19:07:00 +0800

    提交

--- src/main/java/com/tao/wztsnn/WztsnnApplication.java
+++ src/main/java/com/tao/wztsnn/WztsnnApplication.java
@@ -16,7 +16,6 @@ public class WztsnnApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WztsnnApplication.class, args);
-        System.out.println("测试行不行");
     }
 
 }
