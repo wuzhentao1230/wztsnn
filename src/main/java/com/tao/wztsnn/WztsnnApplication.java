7fbb0205e26a01799c6b4ace8b9e186b187b3f66
wb-wzt445153 <wb-wzt445153@alibaba-inc.com>
2019-01-15 11:02:25 +0800

    提交

--- src/main/java/com/tao/wztsnn/WztsnnApplication.java
+++ src/main/java/com/tao/wztsnn/WztsnnApplication.java
@@ -2,8 +2,14 @@ package com.tao.wztsnn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+//开启事务
+@EnableTransactionManagement
+//可以对某个方法添加线程池
+@EnableAsync
 public class WztsnnApplication {
 
     public static void main(String[] args) {
