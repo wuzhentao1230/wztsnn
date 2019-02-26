6e6e0c173cf0acf90ba6308d316230d5e35b5069
wb-wzt445153 <wb-wzt445153@alibaba-inc.com>
2019-02-26 11:07:13 +0800

    添加类

---
+++ src/main/java/com/tao/wztsnn/rmsystem/entity/ResultBean.java
@@ -0,0 +1,29 @@
+package com.tao.wztsnn.rmsystem.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResultBean {
+    private String desc;
+    private String message;
+    private int status;
+    private Object data;
+    public void makeSuccess(Object data){
+        this.desc = "success";
+        this.status = 200;
+        this.data = data;
+    }
+    public void makeFail(String message){
+        this.desc = "error";
+        this.status = 400;
+        this.message = message;
+    }
+
+    public void makeEmpty(){
+        this.desc = "empty data";
+        this.status = 300;
+    }
+}
+
