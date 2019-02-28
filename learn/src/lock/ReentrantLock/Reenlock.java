a9f9fd5eb0bc8b233800610186a1e0fe6e4967bf
wb-wzt445153 <wb-wzt445153@alibaba-inc.com>
2019-02-28 11:39:18 +0800

    修改lock

--- learn/src/lock/ReentrantLock/Reenlock.java
+++ learn/src/lock/ReentrantLock/Reenlock.java
@@ -51,4 +51,38 @@ public class Reenlock {
     public void finished() {
         System.out.println("Count is: " + count);
     }
+
+
+    public static void main(String[] args) throws Exception {
+
+        final Reenlock reenlock = new Reenlock();
+
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    reenlock.Thread1();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    reenlock.Thread2();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        reenlock.finished();
+    }
 }
