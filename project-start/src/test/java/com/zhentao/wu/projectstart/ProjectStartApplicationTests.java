78794f04d1443e08987efef81af6b27bf4d7c417
wb-wzt445153 <wb-wzt445153@alibaba-inc.com>
2019-05-13 20:49:11 +0800

    提交

--- project-start/src/test/java/com/zhentao/wu/projectstart/ProjectStartApplicationTests.java
+++ project-start/src/test/java/com/zhentao/wu/projectstart/ProjectStartApplicationTests.java
@@ -1,32 +1,32 @@
-package com.zhentao.wu.projectstart;
-
-import com.zhentao.wu.projectstart.executor.AsyncDemo;
-import com.zhentao.wu.projectstart.executor.TaskPoolConfig;
-import com.zhentao.wu.servicewx.service.DemoService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class ProjectStartApplicationTests {
-    @Autowired
-    DemoService demoService;
-
-    @Autowired
-    AsyncDemo asyncDemo;
-    @Test
-    public void contextLoads() {
-//        demoService.query();
-    }
-    @Test
-    public void execcutorTest() throws Exception {
-        asyncDemo.doTaskOne();
-        asyncDemo.doTaskTwo("success");
-        System.out.println(asyncDemo.doTaskThree().get());
-
-    }
-
-}
+//package com.zhentao.wu.projectstart;
+//
+//import com.zhentao.wu.projectstart.executor.AsyncDemo;
+//import com.zhentao.wu.projectstart.executor.TaskPoolConfig;
+//import com.zhentao.wu.servicewx.service.DemoService;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class ProjectStartApplicationTests {
+//    @Autowired
+//    DemoService demoService;
+//
+//    @Autowired
+//    AsyncDemo asyncDemo;
+//    @Test
+//    public void contextLoads() {
+////        demoService.query();
+//    }
+//    @Test
+//    public void execcutorTest() throws Exception {
+//        asyncDemo.doTaskOne();
+//        asyncDemo.doTaskTwo("success");
+//        System.out.println(asyncDemo.doTaskThree().get());
+//
+//    }
+//
+//}
